66a2ea0dd50e3c29e0d0d6d8c0df892906063b24
Brian Cosgrove <cosgroveb@gmail.com>
2026-03-26 23:34:12 +0000

    Add clnkr formula for v0.8.0

---
+++ Formula/clnkr.rb
@@ -0,0 +1,21 @@
+class Clnkr < Formula
+  desc "A minimal coding agent CLI"
+  homepage "https://github.com/clnkr-ai/clnkr"
+  url "https://github.com/clnkr-ai/clnkr/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "570a12d44add83bd26016e81ced38e629c417ec75e319d9fe133f0601476bea7"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build-all", "VERSION=#{version}"
+    bin.install "clnkr"
+    bin.install "clnku"
+    bin.install_symlink "clnkr" => "clnk"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/clnkr --version")
+    assert_match version.to_s, shell_output("#{bin}/clnku --version")
+  end
+end
